88eeb4e675d762db38591271c59f4fab8f45964f
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:25:02 -0300

    Update test_interruption.lua

--- tests/test_interruption.lua
+++ tests/test_interruption.lua
@@ -1,8 +1,3 @@
--- ##########################################################
--- # Maintainer: Mr. Charkuils                              #
--- # URL:        https://github.com/charkuils/nvim-spinetta #
--- ##########################################################
-
 -- NOTE: To interrupt the job being executed, press Ctrl-C
 
 local spinetta = require'spinetta'
