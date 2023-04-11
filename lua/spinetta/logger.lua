e7e856a7aa467dbd0cc27cf973fc4e7f34f8f18b
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:24:36 -0300

    Update logger.lua

--- lua/spinetta/logger.lua
+++ lua/spinetta/logger.lua
@@ -1,8 +1,3 @@
--- ##########################################################
--- # Maintainer: Mr. Charkuils                              #
--- # URL:        https://github.com/charkuils/nvim-spinetta #
--- ##########################################################
-
 local M = {}
 
 local function logger(plugin_name, msg)
